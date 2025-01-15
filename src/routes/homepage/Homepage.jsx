485a231cc2b632b6f0db53d20c114291772e84ce
Rashi <rashi1704baranwal@gmail.com>
2025-01-15 16:01:20 +0530

    feat: separate pages created and routing set

---
+++ src/routes/homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import './homepage.css'
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div className='homepage'>
+      HomePage
+    </div>
+  )
+}
+
+export default Homepage
